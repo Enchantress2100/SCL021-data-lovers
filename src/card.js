8361637503577f4d5071c3f999d99b45d16e82a2
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-07-25 16:47:05 -0400

    for each que nos trae info de todas las peliculas
    Co-authored-by: bifurcaria <bifurcaria@users.noreply.github.com>

--- src/card.js
+++ src/card.js
@@ -18,14 +18,48 @@ for (i = 0; i < menu.length; i++) {
 }
 
 //obtener data de las peliculas (titulo, release date, director, producer, description, rate)
-data.films.forEach((element) => {
-document.getElementById("info").innerHTML +=
-`<h1>Title: ${element.title}</h1>
+
+/*data.films.forEach((element) => {
+fetch(`http://localhost:3000/card?title=${element.title}`).then(function (response) {
+  document.getElementById("info").innerHTML +=
+  `<h1>Title: ${element.title}</h1>
 <h2>Release date: ${element.release_date}</h2>
 <p>Director: ${element.director}<p>
 <p>Producer: ${element.producer}</p>
 <p>Description: ${element.description}</p>
 <p>Rate: ${element.rt_score}</p>`;
 });
+});*/
+
+data.films.forEach((element) => {
+  fetch(`http://localhost:3000/card?title=${element.title}`).then(function (response) {
+    document.getElementById("info").innerHTML +=
+    `<h1>Title: ${element.title}</h1>
+  <h2>Release date: ${element.release_date}</h2>
+  <p>Director: ${element.director}<p>
+  <p>Producer: ${element.producer}</p>
+  <p>Description: ${element.description}</p>
+  <p>Rate: ${element.rt_score}</p>`;
+  });
+  });
+  
+  Object.keys(my_criteria).every(function(c) {
+    return obj[c] == my_criteria[c];
+  });
 
 
+/*filter(word => word.length > 6);
+
+
+let dataMovie = data.films.filter((title===data.films.title) => {
+  fetch(`http://localhost:3000/card?title=${element.title}`).then(function (response) {
+    document.getElementById("info").innerHTML +=
+      `<h1>Title: ${element.title}</h1>
+<h2>Release date: ${element.release_date}</h2>
+<p>Director: ${element.director}<p>
+<p>Producer: ${element.producer}</p>
+<p>Description: ${element.description}</p>
+<p>Rate: ${element.rt_score}</p>`;
+  })
+})
+return dataMovie*/
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import movies from "./data/ghibli/titulos.js";
 
 
 
-//iteracion a traves del array titulos.js para desplegar imagenes y titulos en index.html.
+//iteracion a traves del array titulos.js para desplegar imagenes y titulos en index.html. Agregar titulo de la pelicula como parametro url para establecer su ruta.
 movies.films.forEach((element) => {
-  document.getElementById("containerBox").innerHTML +=
-`<div><a class="link1" href="./card.html"><img class="picture" src=" ${element.img} "</div><h2 class="title"> ${element.title}</h2></a>`;
+  document.getElementById(
+    "containerBox"
+  ).innerHTML += `<div><a class="link1" href="./card?title=${element.title}"><img class="picture" src=" ${element.img} "</div><h2 class="title"> ${element.title}</h2></a>`;
 });
-
