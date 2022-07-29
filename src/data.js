581fb500b0a0c160b11ca4d57e22b153afea646b
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-07-29 14:08:54 -0400

    realizacion banner, ordenar datos en funcion sortFilms con result
    Co-authored-by: bifurcaria <bifurcaria@users.noreply.github.com>

--- src/data.js
+++ src/data.js
@@ -1,6 +1,24 @@
-export const sortFilms = (films) => {
-  
+import data from "./data/ghibli/ghibli.js";
+export const sortFilms = (films, criteria, asc) => {
+ 
+let result = films.sort(function (a, b) {
+//para ordenar valores numericos (criteria corresponde al parametro que se va a filtrar)
+  if (!isNaN(Number(a[criteria])))
+  {a = new Number(a[criteria])
+   b = new Number(b[criteria])}
+   else {
+//para ordenar valores tipo string
+         a = a[criteria].toUpperCase(),
+         b = b[criteria].toUpperCase(); }
+   return a == b ? 0 : a > b ? 1 : -1; 
+ })
+//para determinar el orden (ascendente por defecto)     
+if (!asc) {
+  return result.reverse()
 }
+  return result
+}
+
 export const anotherExample = () => {
   return "OMG";
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import { sortFilms } from "./data.js";
 import data from "./data/ghibli/ghibli.js";
 import movies from "./data/ghibli/titulos.js";
 
-
-
 //iteracion a traves del array titulos.js para desplegar imagenes y titulos en index.html. Agregar titulo de la pelicula como parametro url para establecer su ruta.
 movies.films.forEach((element) => {
-  document.getElementById(
-    "containerBox"
-  ).innerHTML += `<div><a class="link1" href="./card?id=${element.id}"><img class="picture" src=" ${element.img} "</div><h2 class="title"> ${element.title}</h2></a>`;
+  document.getElementById("containerBox").innerHTML += `<div><a class="link1" href="./card?id=${element.id}"><img class="picture" src=" ${element.img} "</div><h2 class="title"> ${element.title}</h2></a>`;
 });
